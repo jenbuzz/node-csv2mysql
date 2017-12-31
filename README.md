0740396bf5060cfe3321b87d761e6c766c88c135
dan-lyn <daniel_lynge@hotmail.com>
2017-12-31 09:16:40 +0100

    using prompt to request mysql password

--- README.md
+++ README.md
@@ -4,15 +4,16 @@ This node.js script can import data from a csv file into a MySQL table.
 ## Usage
 Run "node node-csv2mysql.js" with the required options.
 
+Enter your MySQL password in the prompt that appears after starting the script.
+
 ### Example
-``node node-csv2mysql.js --host 127.0.0.1 --user root --pw 1234 --db csv2mysql --table csv2mysql --file example.csv``
+``node node-csv2mysql.js --host 127.0.0.1 --user root --db csv2mysql --table csv2mysql --file example.csv``
 
 ### Options
 |Name|Description|Default value|
 |---|---|---|
 |host|MySQL host address|127.0.0.1|
 |user|MySQL username|root|
-|pw|MySQL password||
 |db|MySQL database name||
 |table|MySQL table name||
 |file|Name of csv file to import||
--- node-csv2mysql.js
+++ node-csv2mysql.js
@@ -2,6 +2,15 @@ const fs = require('fs');
 const csv = require('csv-parse');
 const mysql = require('mysql');
 
+const prompt = require('prompt');
+const promptProperties = [
+    {
+      name: 'password',
+      description: 'Enter your MySQL password',
+      hidden: true
+    }
+];
+
 const chalk = require('chalk');
 const log = console.log;
 
@@ -9,7 +18,6 @@ const argv = require('minimist')(process.argv.slice(2));
 
 const host = argv.host !== undefined ? argv.host : '127.0.0.1';
 const user = argv.user !== undefined ? argv.user : 'root';
-const pw = argv.pw !== undefined ? argv.pw+'' : '';
 const db = argv.db !== undefined ? argv.db : '';
 const table = argv.table !== undefined ? argv.table : '';
 const file = argv.file !== undefined ? argv.file : '';
@@ -30,92 +38,102 @@ if (!file) {
     return;
 }
 
-const connection = mysql.createConnection({
-    host: host,
-    user: user,
-    password: pw,
-    database: db
-});
+prompt.start();
+prompt.get(promptProperties, function (err, result) {
+    if (err) {
+        log(chalk.bold.red('Error: ') + chalk.red('Password input failed!'));
+        throw err; 
+    }
 
-let parser = csv({delimiter: delimiter}, function(err, data) {
-    let insertQuery = 'INSERT INTO ' + table + ' SET ?';
+    const pw = result.password;
 
-    let fields = data.shift();
+    let connection = mysql.createConnection({
+        host: host,
+        user: user,
+        password: pw,
+        database: db
+    });
 
-    for (let i = 0; i < data.length; i++) {
-        let newEntry = {};
+    let parser = csv({delimiter: delimiter}, function(err, data) {
+        let insertQuery = 'INSERT INTO ' + table + ' SET ?';
 
-        let relationTableAndCols = [];
+        let fields = data.shift();
 
-        for (let j = 0; j < fields.length; j++) {
-            let isRelation = fields[j].match(/\[(.*?)\]/);
-            if (isRelation && isRelation.length >= 2) {
-                let relationData = isRelation[1].split('|');
-                if (relationData && relationData.length === 3) {
-                    relationTableAndCols[j] = {
-                        table: relationData[0],
-                        column_x: relationData[1],
-                        column_y: relationData[2]
-                    };
+        for (let i = 0; i < data.length; i++) {
+            let newEntry = {};
 
-                    continue;
+            let relationTableAndCols = [];
+
+            for (let j = 0; j < fields.length; j++) {
+                let isRelation = fields[j].match(/\[(.*?)\]/);
+                if (isRelation && isRelation.length >= 2) {
+                    let relationData = isRelation[1].split('|');
+                    if (relationData && relationData.length === 3) {
+                        relationTableAndCols[j] = {
+                            table: relationData[0],
+                            column_x: relationData[1],
+                            column_y: relationData[2]
+                        };
+
+                        continue;
+                    }
                 }
+
+                newEntry[fields[j].toLowerCase()] = data[i][j];
             }
 
-            newEntry[fields[j].toLowerCase()] = data[i][j];
-        }
+            const dataIndex = i;
 
-        const dataIndex = i;
+            let query = connection.query(insertQuery, newEntry, function (err, result) {
+                if (err) {
+                    log(chalk.bold.red('Error: ') + chalk.red(err.sqlMessage));
+                    throw err;
+                }
 
-        let query = connection.query(insertQuery, newEntry, function (err, result) {
-            if (err) {
-                log(chalk.bold.red('Error: ') + chalk.red(err.sqlMessage));
-                throw err;
-            }
+                let insertId = result.insertId;
 
-            let insertId = result.insertId;
+                for (let k = 0; k < relationTableAndCols.length; k++) {
+                    if (relationTableAndCols[k] !== undefined) {
+                        let relData = relationTableAndCols[k];
+                        let relValue = data[dataIndex][k].split('-');
 
-            for (let k = 0; k < relationTableAndCols.length; k++) {
-                if (relationTableAndCols[k] !== undefined) {
-                    let relData = relationTableAndCols[k];
-                    let relValue = data[dataIndex][k].split('-');
+                        if (relValue.length > 0) {
+                            for (let l = 0; l < relValue.length; l++) {
+                                let relEntry = {};
+                                relEntry[relData.column_x] = insertId;
+                                relEntry[relData.column_y] = relValue[l];
 
-                    if (relValue.length > 0) {
-                        for (let l = 0; l < relValue.length; l++) {
-                            let relEntry = {};
-                            relEntry[relData.column_x] = insertId;
-                            relEntry[relData.column_y] = relValue[l];
+                                let relQuery = 'INSERT INTO ' + relData.table.toLowerCase() + ' SET ?';
 
-                            let relQuery = 'INSERT INTO ' + relData.table.toLowerCase() + ' SET ?';
-
-                            let query = connection.query(relQuery, relEntry, function (err, result) {
-                                if (err) {
-                                    log(chalk.bold.red('Error: ') + chalk.red(err.sqlMessage));
-                                    throw err;
-                                }
-                            });
-                            log(chalk.green(query.sql));
+                                let query = connection.query(relQuery, relEntry, function (err, result) {
+                                    if (err) {
+                                        log(chalk.bold.red('Error: ') + chalk.red(err.sqlMessage));
+                                        throw err;
+                                    }
+                                });
+                                log(chalk.green(query.sql));
+                            }
                         }
                     }
                 }
-            }
-        });
-        log(chalk.green(query.sql));
-    }
-});
-
-fs.access(__dirname + '/' + file, fs.constants.R_OK, (err) => {
-    if (err) {
-        log(chalk.bold.red('Error: ') + chalk.red('No access to file!'));
-        return;
-    }
-
-    connection.connect(function(err) {
-        if (err) {
-            log(chalk.bold.red('Error: ') + chalk.red('Could not connect to database!'));
-            return;
+            });
+            log(chalk.green(query.sql));
         }
+    });
 
-        fs.createReadStream(__dirname + '/' + file).pipe(parser);
+    fs.access(__dirname + '/' + file, fs.constants.R_OK, (err) => {
+        if (err) {
+            log(chalk.bold.red('Error: ') + chalk.red('No access to file!'));
+            return;
+        }    
+
+        connection.connect(function(err) {
+            if (err) {
+                log(chalk.bold.red('Error: ') + chalk.red('Could not connect to database!'));
+                return;
+            }
+
+            fs.createReadStream(__dirname + '/' + file).pipe(parser);
+        });
     });
 });
