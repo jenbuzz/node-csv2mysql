12c41464af3c80202be6824ca5ec760ce1f2c279
dan-lyn <daniel_lynge@hotmail.com>
2018-06-15 20:13:56 +0200

    fixed linting issues

--- csv2mysql.js
+++ csv2mysql.js
@@ -1,31 +1,31 @@
 const fs = require('fs');
 const csv = require('csv-parse');
 const mysql = require('mysql');
-
 const prompt = require('prompt');
-const promptProperties = [
-    {
-      name: 'password',
-      description: 'Enter your MySQL password',
-      hidden: true
-    }
-];
+const chalk = require('chalk');
+const parsearg = require('minimist');
+
+const promptProperties = [{
+    name: 'password',
+    description: 'Enter your MySQL password',
+    hidden: true
+}];
 
 // Logging
-const chalk = require('chalk');
-const log = console.log;
+const {log} = console;
 const errorMsg = chalk.bold.red('Error: ');
 
 // Arguments
-const argv = require('minimist')(process.argv.slice(2));
-const host = argv.host !== undefined ? argv.host : '127.0.0.1';
-const user = argv.user !== undefined ? argv.user : 'root';
-const db = argv.db !== undefined ? argv.db : '';
-const table = argv.table !== undefined ? argv.table : '';
-const file = argv.file !== undefined ? argv.file : '';
-const delimiter = argv.delimiter !== undefined ? argv.delimiter : ',';
 
-if (!db) {
+const argv = parsearg(process.argv.slice(2));
+const host = argv.host === undefined ? '127.0.0.1' : argv.host;
+const user = argv.user === undefined ? 'root' : argv.user;
+const database = argv.db === undefined ? '' : argv.db;
+const table = argv.table === undefined ? '' : argv.table;
+const file = argv.file === undefined ? '' : argv.file;
+const delimiter = argv.delimiter === undefined ? ',' : argv.delimiter;
+
+if (!database) {
     throwMissingArgument('db');
 }
 
@@ -43,24 +43,29 @@ function throwMissingArgument (type) {
 }
 
 prompt.start();
-prompt.get(promptProperties, function (err, result) {
+prompt.get(promptProperties, (err, result) => {
     if (err) {
         log(errorMsg + chalk.red('Password input failed!'));
-        throw err; 
+        throw err;
     }
 
-    const pw = result.password;
+    const {password} = result;
 
     // Setup MySQL connection
     const connection = mysql.createConnection({
-        host: host,
-        user: user,
-        password: pw,
-        database: db
+        host,
+        user,
+        password,
+        database
     });
 
     // Start parsing the csv file
-    const parser = csv({delimiter: delimiter}, function(err, data) {
+    const parser = csv({delimiter}, (err, data) => {
+        if (err) {
+            log(errorMsg + chalk.red('File parsing failed'));
+            throw err;
+        }
+
         const insertQuery = 'INSERT INTO ' + table + ' SET ?';
 
         // Getting the first row containing the table column names
@@ -79,8 +84,8 @@ prompt.get(promptProperties, function (err, result) {
                     if (relationData && relationData.length === 3) {
                         relationTableAndCols[j] = {
                             table: relationData[0],
-                            column_x: relationData[1],
-                            column_y: relationData[2]
+                            columnX: relationData[1],
+                            columnY: relationData[2]
                         };
 
                         continue;
@@ -94,13 +99,13 @@ prompt.get(promptProperties, function (err, result) {
             const dataIndex = i;
 
             // Insert the data
-            const query = connection.query(insertQuery, newEntry, function (err, result) {
+            const query = connection.query(insertQuery, newEntry, (err, result) => {
                 if (err) {
                     log(errorMsg + chalk.red(err.sqlMessage));
                     throw err;
                 }
 
-                const insertId = result.insertId;
+                const {insertId} = result;
 
                 // Add relationship data if any specified
                 for (let k = 0; k < relationTableAndCols.length; k++) {
@@ -111,12 +116,12 @@ prompt.get(promptProperties, function (err, result) {
                         if (relValue.length > 0) {
                             for (let l = 0; l < relValue.length; l++) {
                                 const relEntry = {};
-                                relEntry[relData.column_x] = insertId;
-                                relEntry[relData.column_y] = relValue[l];
+                                relEntry[relData.columnX] = insertId;
+                                relEntry[relData.columnY] = relValue[l];
 
                                 const relQuery = 'INSERT INTO ' + relData.table.toLowerCase() + ' SET ?';
 
-                                const query = connection.query(relQuery, relEntry, function (err, result) {
+                                const query = connection.query(relQuery, relEntry, err => {
                                     if (err) {
                                         log(errorMsg + chalk.red(err.sqlMessage));
                                         throw err;
@@ -133,14 +138,14 @@ prompt.get(promptProperties, function (err, result) {
     });
 
     // Check for valid file source
-    fs.access(__dirname + '/' + file, fs.constants.R_OK, (err) => {
+    fs.access(__dirname + '/' + file, fs.constants.R_OK, err => {
         if (err) {
             log(errorMsg + chalk.red('No access to file!'));
             return;
-        }    
+        }
 
         // Connect to database
-        connection.connect(function(err) {
+        connection.connect(err => {
             if (err) {
                 log(errorMsg + chalk.red('Could not connect to database!'));
                 return;
